52f3aa1ed825bde64a5f73274ec32c03fc3b31f5
GreenNugget <naomi.snchz99@gmail.com>
2021-06-14 19:01:35 -0500

    :card_file_box: Typos in the db fixed

--- src/main/resources/dump.sql
+++ src/main/resources/dump.sql
@@ -42,7 +42,7 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `users` (
   `user_id` INT NOT NULL AUTO_INCREMENT,
   `email` VARCHAR(45) NOT NULL UNIQUE,
-  `password` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(300) NOT NULL,
   `fullname` VARCHAR(45) NOT NULL,
   `lastname` VARCHAR(45) NOT NULL,
   `created_at` DATETIME NOT NULL,
@@ -81,11 +81,11 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table readList
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `readList` (
-  `readList_id` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `readlist` (
+  `readlist_id` INT NOT NULL AUTO_INCREMENT,
   `user_id` INT NOT NULL,
   `name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`readList_id`),
+  PRIMARY KEY (`readlist_id`),
     FOREIGN KEY (`user_id`)
     REFERENCES `users` (`user_id`)
     ON DELETE NO ACTION
@@ -96,13 +96,13 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 -- Table readLists-books
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `readLists-books` (
-  `readList_id` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `readlists_books` (
+  `readlist_id` INT NOT NULL,
   `book_id` INT NOT NULL,
   `readlist_book_id` INT NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`readlist_book_id`),
-    FOREIGN KEY (`readList_id`)
-    REFERENCES `readList` (`readList_id`)
+    FOREIGN KEY (`readlist_id`)
+    REFERENCES `readList` (`readlist_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
     FOREIGN KEY (`book_id`)
@@ -115,3 +115,14 @@ ENGINE = InnoDB;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+INSERT INTO `type_user` (`type_user_id`, `type`) VALUES
+(1, 'ADMIN'),
+(2, 'PREMIUM'),
+(3, 'FREE');
+
+
+INSERT INTO `users` (`user_id`, `email`, `password`, `fullname`, `lastname`, `created_at`, `updated_at`, `token`, `type_user`) VALUES
+(1, 'admin@gmail.com', '$2a$10$IIfInBBRThZvEQ2Odyw63.06uj0tyN8CjVsztLCrmeIopVYScWGmO', 'AdminName', 'AdminLastname', '2021-06-13 12:29:53', '2021-06-13 12:29:53', NULL, 1);
+
+INSERT INTO `booktify`.`books`  VALUES ('1', 'demo', 'demo', 'demo', 'demo', 'demo', '2021-06-13', '100', 'lan', 'https', 'https', 'https');
