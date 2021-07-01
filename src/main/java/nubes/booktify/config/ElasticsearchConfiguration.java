7aed9a4537f9d38dba68634433e79dcafb327d76
davidita118 <david.isaac.iker@hotmail.com>
2021-07-01 02:05:15 -0500

    feat: added config for elasticsearch

---
+++ src/main/java/nubes/booktify/config/ElasticsearchConfiguration.java
@@ -0,0 +1,34 @@
+package nubes.booktify.config;
+
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+
+@Configuration
+public class ElasticsearchConfiguration extends AbstractElasticsearchConfiguration {
+    
+    @Value("${elasticsearch.port}")
+    private Integer port;
+
+    @Bean
+    @Override
+    public RestHighLevelClient elasticsearchClient() {
+        ClientConfiguration clientConfiguration 
+        = ClientConfiguration.builder()
+        .connectedTo("localhost:"+this.port)
+        .build();
+
+        return RestClients.create(clientConfiguration).rest();
+    }
+
+    @Bean
+    public ElasticsearchOperations elasticsearchTemplate() {
+        return new ElasticsearchRestTemplate(elasticsearchClient());
+    }
+}
