57796bd05ee6d3fabb11afa887139f509f2740fd
davidita118 <david.isaac.iker@hotmail.com>
2021-07-05 18:29:48 -0500

    feat: added role

--- src/main/java/nubes/booktify/rest/FileRest.java
+++ src/main/java/nubes/booktify/rest/FileRest.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.MediaTypeFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -21,6 +22,7 @@ public class FileRest {
   @Autowired
   private FileStorageService fileStorageService;
 
+  @PreAuthorize("hasRole('ROLE_ADMIN')")
   @PostMapping("/uploadFile/{id}")
   public ResponseEntity<UploadFileResponse> uploadFile(@RequestParam("file") MultipartFile file,
       @PathVariable Integer id) {
--- src/main/java/nubes/booktify/rest/OpenLibraRest.java
+++ src/main/java/nubes/booktify/rest/OpenLibraRest.java
@@ -2,6 +2,7 @@ package nubes.booktify.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,7 @@ public class OpenLibraRest {
   @Autowired
   private OpenLibraService openLibraService;
 
+  @PreAuthorize("hasRole('ROLE_ADMIN')")
   @GetMapping("/openlibra/libros")
   public ResponseEntity<BookOpenLibra[]> getLibrosOpenLibra(@RequestParam(name = "name") String bookName,
       @RequestParam(name = "author", required = false) String author) {
@@ -26,6 +28,7 @@ public class OpenLibraRest {
     return ResponseEntity.ok().body(books);
   }
 
+  @PreAuthorize("hasRole('ROLE_ADMIN')")
   @PostMapping("/openlibra/libros/{id}")
   public ResponseEntity<BookOpenLibra[]> postLibrosOpenLibra(@PathVariable(name = "id") Integer bookId) {
     openLibraService.getBookOpenLibraById(bookId);
