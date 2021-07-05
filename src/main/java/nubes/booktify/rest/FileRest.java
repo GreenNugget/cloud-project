cabc9862b9b10f16760c32b88f596ad14f60469b
Ariel May <aemd199711@gmail.com>
2021-07-05 18:02:23 -0500

    feat: upload and download pdf

---
+++ src/main/java/nubes/booktify/rest/FileRest.java
@@ -0,0 +1,39 @@
+package nubes.booktify.rest;
+
+import java.net.MalformedURLException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.MediaTypeFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import nubes.booktify.model.UploadFileResponse;
+import nubes.booktify.service.FileStorageService;
+
+@RestController
+@RequestMapping("/api/books")
+public class FileRest {
+
+  @Autowired
+  private FileStorageService fileStorageService;
+
+  @PostMapping("/uploadFile/{id}")
+  public ResponseEntity<UploadFileResponse> uploadFile(@RequestParam("file") MultipartFile file,
+      @PathVariable Integer id) {
+    UploadFileResponse response = fileStorageService.storeFile(file, id);
+
+    return ResponseEntity.status(HttpStatus.CREATED).body(response);
+  }
+
+  @GetMapping("/downloadFile/{fileName}")
+  public ResponseEntity<UrlResource> downloadFile(@PathVariable String fileName) throws MalformedURLException {
+    UrlResource pdf = fileStorageService.loadFileAsResource(fileName);
+
+    return ResponseEntity.status(HttpStatus.OK)
+        .contentType(MediaTypeFactory.getMediaType(pdf).orElse(MediaType.APPLICATION_OCTET_STREAM)).body(pdf);
+  }
+}
